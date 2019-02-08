d59f673a7663d23b02f58d11c4d5783c037a0a4a
Sinan Deger <sinandeger@gmail.com>
2019-02-08 11:05:46 -0600

    Update keras_implementation.py

--- keras_implementation.py
+++ keras_implementation.py
@@ -132,32 +132,6 @@ for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
 
 plot_losses = kerfunc.PlotLosses()
 
-"""CONVNET MODEL IS INCOMPLETE"""
-
-def keras_conv_net_model(input_shape):
-
-    X_input = Input(input_shape)
-
-    # Zero-Padding: pads the border of X_input with zeroes
-    X = ZeroPadding2D((3, 3))(X_input)
-
-    # CONV -> BN -> RELU Block applied to X
-    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
-    X = BatchNormalization(axis=3, name='bn0')(X)
-    X = Activation('relu')(X)
-
-    outputs = Conv2D(1, (1, 1), activation='sigmoid')(X)
-
-    # # MAXPOOL
-    # X = MaxPooling2D((2, 2), name='max_pool')(X)
-    #
-    # # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
-    # X = Flatten()(X)
-    # X = Dense(1, activation='sigmoid', name='fc')(X)
-
-    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
-    model = Model(inputs=X_input, outputs=outputs, name='Keras-1LayerModel')
-    return model
 
 def keras_u_net_model(input_shape):
 
